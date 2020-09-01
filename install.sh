1ceaba19aa79ea3d503df34c5b12a6a8d47a4efe
Logan Connolly <logan.connolly@protonmail.com>
2020-09-01 09:51:33 +0200

    Add wallpaper and add venv when installing

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo pacman -S \
   base-devel \
   ctags \
   fd \
+  firefox \
   neovim \
   nodejs \
   npm \
@@ -18,3 +19,10 @@ yay -S polybar nerd-fonts-source-code-pro
 
 sudo chsh -s $(which zsh)
 export TERM=allacrity
+
+mkdir $HOME/.envs && cd $HOME/.envs
+python -m venv neovim \
+  && source neovim/bin/activate \
+  && pip install -U pip setuptools wheel \
+  && pip install -r $HOME/github/dotfiles/nvim/requirements.txt \
+  && deactivate
