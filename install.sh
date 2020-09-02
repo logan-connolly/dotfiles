d6e55946f5ef4fc3fac71995ad95943b9c7b0b6e
Logan Connolly <logan.connolly@protonmail.com>
2020-09-02 08:19:42 +0200

    Organize install script

--- install.sh
+++ install.sh
@@ -1,29 +1,71 @@
 #!/bin/bash
 
-sudo pacman -S \
-  alacritty \
-  adobe-source-code-pro-fonts \
-  base-devel \
-  ctags \
-  fd \
-  firefox \
-  neovim \
-  nodejs \
-  npm \
-  rofi \
-  python-pynvim \
-  ttf-font-awesome \
-  yay \
-  zsh \
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
 
-yay -S polybar nerd-fonts-source-code-pro
 
-sudo chsh -s $(which zsh)
-export TERM=allacrity
+install_pacman() {
+  sudo pacman -S \
+    alacritty \
+    adobe-source-code-pro-fonts \
+    base-devel \
+    ctags \
+    fd \
+    firefox \
+    rofi \
+    ttf-font-awesome \
+    yay \
+    zsh
+}
 
-mkdir $HOME/.envs && cd $HOME/.envs
-python -m venv neovim \
-  && source neovim/bin/activate \
-  && pip install -U pip setuptools wheel \
-  && pip install -r $HOME/github/dotfiles/nvim/requirements.txt \
-  && deactivate
+install_aur() {
+  yay -S \
+    nerd-fonts-source-code-pro \
+    polybar \
+    xcwd-git
+}
+
+change_shell() {
+  sudo chsh -s $(which zsh)
+  export TERM=allacrity
+}
+
+install_nvim() {
+  sudo pacman -S \ 
+    neovim \
+    nodejs \
+    npm \
+    python-pynvim 
+
+  if [ 1 -d "$HOME/.envs" ]
+    then
+      echo "Creating .envs directory under home."
+      mkdir $HOME/.envs
+  fi
+
+  python -m venv $HOME/.envs/neovim \
+    && source $HOME/.envs/neovim/bin/activate \
+    && pip install -U pip setuptools wheel \
+    && pip install -r ./nvim/requirements.txt \
+    && deactivate
+}
+
+install_docker() {
+  sudo pacman -S docker docker-compose \
+    && sudo usermod -aG docker manjaro \
+    && sudo systemctl enable docker.service \
+    && sudo systemctl start docker.service
+}
+
+
+echo "Installing pacman pacakges ..."
+install_pacman
+echo "Installing AUR packages ..."
+install_aur
+echo "Changing shell and terminal ..."
+change_shell
+echo "Install neovim ..."
+install_nvim
+echo "Installing docker ..."
+install_docker
+echo "Finished."
