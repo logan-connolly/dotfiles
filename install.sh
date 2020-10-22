d6bdd9ecf2eb066986912117647d1eea5560f376
Logan Connolly <logan.connolly@protonmail.com>
2020-10-22 15:03:21 +0200

    Update installer by separating out functions

--- install.sh
+++ install.sh
@@ -25,24 +25,23 @@ install_aur() {
     xcwd-git
 }
 
-change_shell() {
-  sudo chsh -s $(which zsh)
-  export TERM=allacrity
-}
-
 install_nvim() {
   sudo pacman -S \ 
     neovim \
     nodejs \
     npm \
     python-pynvim 
+}
 
+create_venv() {
   if [ 1 -d "$HOME/.envs" ]
     then
       echo "Creating .envs directory under home."
       mkdir $HOME/.envs
   fi
+}
 
+install_py_pkgs() {
   python -m venv $HOME/.envs/neovim3 \
     && source $HOME/.envs/neovim3/bin/activate \
     && pip install -U pip setuptools wheel \
@@ -57,15 +56,23 @@ install_docker() {
     && sudo systemctl start docker.service
 }
 
+change_shell() {
+  chsh -s $(which zsh)
+  export TERM=alacritty
+}
+
 
 echo "Installing pacman pacakges ..."
 install_pacman
 echo "Installing AUR packages ..."
 install_aur
-echo "Changing shell and terminal ..."
-change_shell
 echo "Install neovim ..."
 install_nvim
+echo "Install python packages ..."
+create_venv
+install_py_pkgs
 echo "Installing docker ..."
 install_docker
+echo "Changing shell and terminal ..."
+change_shell
 echo "Finished."
