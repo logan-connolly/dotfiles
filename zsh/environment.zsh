1e885353bbb4b584a86c1f5d2ecf83e92c5b3efe
Logan Connolly <logan.connolly@protonmail.com>
2020-10-24 18:40:01 +0200

    Add default programs env vars

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,10 +1,15 @@
+# default programs
+export EDITOR="nvim"
+export VISUAL="nvim"
+export TERMINAL="alacritty"
+export BROWSER="firefox"
+export READER="zathura"
+
+# edit PATH
+export PATH=~/bin:$PATH
+export PATH=~/.npm-global/bin:$PATH
+
 # fzf autocompletion
 [ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
 [ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
 export FZF_DEFAULT_OPS="--extended"
-
-# add shell functions to path
-export PATH=~/bin:$PATH
-
-# add npm global dir to home
-export PATH=~/.npm-global/bin:$PATH
