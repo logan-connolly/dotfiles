8f2f2e443f86b7613ad132946eb4f44715179834
Logan Connolly <logan.connolly@protonmail.com>
2021-03-14 13:12:22 +0100

    Clone repositories dynamically with install script

--- install.sh
+++ install.sh
@@ -2,54 +2,61 @@
 
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
-DOT_DIR=$HOME/github/dotfiles
+
+# Paremeters and Helper Commands
+DOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+MKDIR=mkdir -p
+LN=ln -vsf
+LNDIR=ln -vs
+PKGINSTALL=yay --noconfirm -S --needed
 
 install_pacman_packages() {
-  yay -S base-devel
-  yay -S --needed - < pacman/pkglist.txt
-  yay -S --needed - < pacman/aur.txt
+  $PKGINSTALL base-devel
+  $PKGINSTALL - < pacman/pkglist.txt
+  $PKGINSTALL - < pacman/aur.txt
 }
 
 install_npm_packages() {
-  mkdir -p ~/.npm-global \
-    && npm config set prefix '~/.npm-global' \
-    && export PATH=~/.npm-global/bin:$PATH \
-    && npm install -g \
-      eslint \
-      live-server \
-      neovim \
-      prettier \
-      typescript-language-server \
-      vls
+  $MKDIR ~/.npm-global
+  npm config set prefix '~/.npm-global'
+  export PATH=~/.npm-global/bin:$PATH
+  pkgs="eslint live-server neovim prettier typescript-language-server vls"
+  npm install -g $pkgs
 }
 
-link_configs() {
-  # symlink of entire config files
-  ln -sf $DOT_DIR/config/.flake8 ~/.flake8
-  ln -sf $DOT_DIR/config/.gitconfig ~/.gitconfig
-  ln -sf $DOT_DIR/config/.inputrc ~/.inputrc
-  ln -sf $DOT_DIR/config/.pylintrc ~/.pylintrc
-  ln -sf $DOT_DIR/config/.xinitrc ~/.xinitrc
-  ln -sf $DOT_DIR/config/.Xresources ~/.Xresources
-  ln -sf $DOT_DIR/config/.zshrc ~/.zshrc
-  ln -sf $DOT_DIR/config/.zprofile ~/.zprofile
+symlink_files() {
+  $LN $DOTDIR/config/alacritty.yml ~/.config/alacritty.yml
+  $LN $DOTDIR/config/.flake8 ~/.flake8
+  $LN $DOTDIR/config/.gitconfig ~/.gitconfig
+  $LN $DOTDIR/config/.inputrc ~/.inputrc
+  $LN $DOTDIR/config/.mimeapps.list ~/.config/.mimeapps.list
+  $LN $DOTDIR/config/.pylintrc ~/.pylintrc
+  $LN $DOTDIR/config/.xinitrc ~/.xinitrc
+  $LN $DOTDIR/config/.Xresources ~/.Xresources
+  $LN $DOTDIR/config/.zshrc ~/.zshrc
+  $LN $DOTDIR/config/.zprofile ~/.zprofile
+}
 
-  # symlink of entire config directories
-  ln -sf $DOT_DIR/config/alacritty.yml ~/.config
-  ln -sf $DOT_DIR/config/.mimeapps.list ~/.config
-  ln -sf $DOT_DIR/config/feh ~/.config
-  ln -sf $DOT_DIR/config/i3 ~/.config
-  ln -sf $DOT_DIR/config/nvim ~/.config
-  ln -sf $DOT_DIR/config/qutebrowser ~/.config
-  ln -sf $DOT_DIR/config/ranger ~/.config
-  ln -sf $DOT_DIR/config/rofi ~/.config
-  ln -sf $DOT_DIR/config/picom ~/.config
-  ln -sf $DOT_DIR/config/polybar ~/.config
-  ln -sf $DOT_DIR/config/zsh ~/.config
+symlink_dirs() {
+  $LNDIR $DOTDIR/bin ~/
+  $LNDIR $DOTDIR/config/feh ~/.config
+  $LNDIR $DOTDIR/config/i3 ~/.config
+  $LNDIR $DOTDIR/config/nvim ~/.config
+  $LNDIR $DOTDIR/config/qutebrowser ~/.config
+  $LNDIR $DOTDIR/config/ranger ~/.config
+  $LNDIR $DOTDIR/config/rofi ~/.config
+  $LNDIR $DOTDIR/config/picom ~/.config
+  $LNDIR $DOTDIR/config/polybar ~/.config
+  $LNDIR $DOTDIR/config/zsh ~/.config
+}
 
-  # symlink misc directories and files
-  ln -sf $DOT_DIR/bin ~/
-  ln -sf $DOT_DIR/themes ~/.themes
+clone_repos() {
+  gruvbox_gtk_repo=https://github.com/jmattheis/gruvbox-dark-gtk.git
+  ranager_devicon_repo=https://github.com/alexanderjeurissen/ranger_devicons.git
+  $MKDIR ~/.themes
+  $MKDIR ~/.config/ranger/plugins
+  git clone $gruvbox_gtk_repo --single-branch ~/.themes
+  git clone $rnager_devicon_repo --single-branch ~/.config/ranger/plugins
 }
 
 enable_services() {
@@ -64,7 +71,12 @@ echo "Installing pacman packages ..."
 install_pacman_packages
 echo "Installing npm neovim globally ..."
 install_npm_packages
-echo "Link configurations ..."
-link_configs
-echo "Enable services"
+echo "Symlinking configuration files ..."
+symlink_files
+echo "Symlinking configuration directories ..."
+symlink_dirs
+echo "Cloning third party repos ..."
+clone_repos
+echo "Enabling system services ..."
 enable_services
+echo "FINISHED: recommend rebooting system"
