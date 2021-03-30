68f8501a8600ea943e3e2fbb7c0776bb1eb2f5ad
Logan Connolly <logan.connolly@protonmail.com>
2021-03-30 22:06:56 +0200

    Add hop for easymotion

---
+++ config/nvim/lua/nvimlua/hop.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1()<cr>", {})
+vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char2()<cr>", {})
--- config/nvim/lua/nvimlua/init.lua
+++ config/nvim/lua/nvimlua/init.lua
@@ -1,3 +1,4 @@
+require('nvimlua.hop')
 require('nvimlua.lsp')
 require('nvimlua.lualine')
 require('nvimlua.telescope')
