49da5a15b8335469eda23b0590d955bffee343c9
Logan Connolly <logan.connolly@protonmail.com>
2021-04-08 01:25:50 +0200

    Function key mappings are not working

--- config/nvim/lua/plugins/fugitive/keybinds.lua
+++ config/nvim/lua/plugins/fugitive/keybinds.lua
@@ -5,6 +5,5 @@ Keybind.g({
 	{ 'n', '<Leader>ga', [[<Cmd>Git fetch --all<CR>]], defaults },
 	{ 'n', '<Leader>gf', [[<Cmd>diffget //2<CR>]], { noremap = false, silent = true } },
 	{ 'n', '<Leader>gj', [[<Cmd>diffget //3<CR>]], { noremap = false, silent = true } },
-	{ 'n', 'F9', [[<Cmd>G commit -a<CR>]], defaults },
-	{ 'n', 'F10', [[<Cmd>G push<CR>]], defaults },
+	{ 'n', '<Leader>gp', [[<Cmd>G push<CR>]], defaults },
 })
