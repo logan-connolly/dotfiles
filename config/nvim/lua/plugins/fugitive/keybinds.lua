558fd9beeac5f4290abc6574a8e247e5aa20783c
Logan Connolly <logan.connolly@protonmail.com>
2021-04-09 10:46:49 +0200

    Change how git fetch and pull

--- config/nvim/lua/plugins/fugitive/keybinds.lua
+++ config/nvim/lua/plugins/fugitive/keybinds.lua
@@ -2,8 +2,8 @@ local defaults = { noremap = true, silent = true }
 
 Keybind.g({
 	{ 'n', '<Leader>gs', [[<Cmd>G<CR>]], { noremap = false, silent = true } },
-	{ 'n', '<Leader>ga', [[<Cmd>Git fetch --all<CR>]], defaults },
 	{ 'n', '<Leader>gf', [[<Cmd>diffget //2<CR>]], { noremap = false, silent = true } },
 	{ 'n', '<Leader>gj', [[<Cmd>diffget //3<CR>]], { noremap = false, silent = true } },
-	{ 'n', '<Leader>gp', [[<Cmd>G push<CR>]], defaults },
+	{ 'n', '<Leader>g[', [[<Cmd>Git fetch --all<CR>]], defaults },
+	{ 'n', '<Leader>g]', [[<Cmd>G push<CR>]], defaults },
 })
