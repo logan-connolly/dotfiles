1bcb9175dfbeb56ce8794f625d4cf7acb1a48e31
Logan Connolly <logan.connolly@protonmail.com>
2021-04-26 09:41:08 +0200

    Add more formatters

--- config/nvim/lua/plugins/format/settings.lua
+++ config/nvim/lua/plugins/format/settings.lua
@@ -1,17 +1,23 @@
+local prettier = { cmd = { "prettier -w" }}
+local prettier_eslint = { cmd = { "prettier -w", "./node_modules/.bin/eslint --fix" }}
+local python = { cmd = { "black", "isort" }}
+local python_markdown = {
+	cmd = { "black" },
+	start_pattern = "^```python$",
+	end_pattern = "^```$",
+	target = "current"
+}
+local remove_whitespace = { cmd = { "sed -i 's/[ \t]*$//'" }}
+
 require "format".setup {
-	["*"] = {
-		{ cmd = { "sed -i 's/[ \t]*$//'" }} -- remove trailing whitespace
-	},
-	python = {
-		{ cmd = { "black", "isort" }}
-	},
-	markdown = {
-		{ cmd = { "prettier -w" }},
-		{
-			cmd = { "black" },
-			start_pattern = "^```python$",
-			end_pattern = "^```$",
-			target = "current"
-		}
-	}
+	["*"] = { remove_whitespace },
+	css = { prettier },
+	html = { prettier },
+	html = { prettier },
+	json = { prettier },
+	yaml = { prettier },
+	markdown = { prettier, python_markdown },
+	javascript = { prettier_eslint },
+	vue	 = { prettier_eslint },
+	python = { python },
 }
