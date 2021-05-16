6770180f91f9d1426ca4145e54434f895058cc68
Logan Connolly <logan.connolly@protonmail.com>
2021-05-16 10:23:38 +0200

    Add autocommand to enable transparent bg in neovim

--- config/nvim/lua/general/commands.lua
+++ config/nvim/lua/general/commands.lua
@@ -1,2 +1,5 @@
 -- highlight yanked text
 vim.cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
+
+--- transparent background
+vim.cmd 'au ColorScheme * hi Normal ctermbg=none guibg=none'
