9d16b91d36a0e4b98f6b2a2e7894808c38449e43
Logan Connolly <logan.connolly@protonmail.com>
2021-06-11 20:46:54 +0200

    Support Lua language server in LSP

--- config/nvim/lua/plugins/lsp/settings.lua
+++ config/nvim/lua/plugins/lsp/settings.lua
@@ -25,11 +25,40 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	}
 )
 
+-- Configure lua language server
+local sumneko_root_path = '~/.local/opt/lua-language-server'
+local sumneko_binary = '/usr/bin/lua-language-server'
+local lua_settings = {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';')
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'}
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+        }
+      }
+    }
+  }
+}
+
 -- LSP servers to support with optional arguments
 local servers = {
   pyright = {},
   tsserver = {},
   vuels = {},
+  sumneko_lua = lua_settings
 }
 
 -- Configure each LSP by attaching completion and status capbilities
--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 clone_repo() {
-  REPO_URL=$1
-  REPO_PATH=$2
-  rm -rf $REPO_PATH && mkdir -p $REPO_PATH
-  git clone $REPO_URL --single-branch $REPO_PATH
+	REPO_URL=$1
+	REPO_PATH=$2
+	rm -rf $REPO_PATH && mkdir -p $REPO_PATH
+	git clone $REPO_URL --single-branch $REPO_PATH
 }
 
 clone_gruvbox_gtk() {
-  gruvbox_gtk_repo=https://github.com/jmattheis/gruvbox-dark-gtk.git
-  gruvbox_gtk_path="$HOME/.themes/gruvbox-dark-gtk"
-  clone_repo $gruvbox_gtk_repo $gruvbox_gtk_path
+	repo=https://github.com/jmattheis/gruvbox-dark-gtk.git
+	path="$HOME/.themes/gruvbox-dark-gtk"
+	clone_repo $repo $path
+}
+
+clone_lua_language_server() {
+	repo=https://github.com/sumneko/lua-language-server.git
+	path="$HOME/.local/opt/lua-language-server"
+	clone_repo $repo $path
 }
 
 clone_gruvbox_gtk
+clone_lua_language_server
