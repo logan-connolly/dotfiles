3d53f42bf1b59dceaea8e6414efe5f3381cf39a2
Logan Connolly <logan.connolly@protonmail.com>
2021-09-07 12:54:04 +0200

    Add changes to telescope utils package :)

--- config/nvim/lua/plugins/telescope/keybinds.lua
+++ config/nvim/lua/plugins/telescope/keybinds.lua
@@ -1,38 +1,38 @@
-local builtin_module = [['telescope.builtin'.]]
-local custom_module = [['plugins.telescope.settings'.]]
-local extension_module = [['telescope'.extensions.]]
+local B = [['telescope.builtin'.]]
+local S = [['plugins.telescope.settings'.]]
+local E = [['telescope'.extensions.]]
 local defaults = { noremap = true, silent = true }
 
-local function telescope_cmd(module, fn)
+local function cmd(module, fn)
 	return '<Cmd>lua require' .. module .. fn .. ' <CR>'
 end
 
 Keybind.g({
 	-- search files
-	{ 'n', '<Leader>ff', telescope_cmd(builtin_module, "find_files({ hidden = true })"), defaults },
-	{ 'n', '<C-p>', telescope_cmd(builtin_module, 'git_files({ hidden = true })'), defaults },
+	{ 'n', '<Leader>ff', cmd(B, "find_files({ hidden = true })"), defaults },
+	{ 'n', '<C-p>', cmd(B, 'git_files({ hidden = true })'), defaults },
 
 	-- search word
-	{ 'n', '<Leader>fg', telescope_cmd(builtin_module, "grep_string({ search = vim.fn.input('> ') })"), defaults },
+	{ 'n', '<Leader>fg', cmd(B, "grep_string({ search = vim.fn.input('> ') })"), defaults },
 
 	-- tpope/vim-vinegar replacement
-	{ 'n', '-', telescope_cmd(builtin_module, "file_browser({ cwd = vim.fn.expand('%:p:h') })"), defaults },
+	{ 'n', '-', cmd(B, "file_browser({ cwd = require'telescope.utils'.buffer_dir() })"), defaults },
 
 	-- search buffers
-	{ 'n', '<Leader>fb', telescope_cmd(builtin_module, 'buffers()'), defaults },
+	{ 'n', '<Leader>fb', cmd(B, 'buffers()'), defaults },
 
 	-- search help pages
-	{ 'n', '<Leader>fh', telescope_cmd(builtin_module, 'help_tags()'), defaults },
+	{ 'n', '<Leader>fh', cmd(B, 'help_tags()'), defaults },
 
 	-- search projects
-	{ 'n', '<Leader>fp', telescope_cmd(extension_module, 'project.project{}'), defaults },
+	{ 'n', '<Leader>fp', cmd(E, 'project.project{}'), defaults },
 
 	-- custom searches
-	{ 'n', '<Leader>fd', telescope_cmd(custom_module, 'search_dotfiles()'), defaults },
-	{ 'n', '<Leader>fP', telescope_cmd(custom_module, 'search_plugins()'), defaults },
+	{ 'n', '<Leader>fd', cmd(S, 'search_dotfiles()'), defaults },
+	{ 'n', '<Leader>fP', cmd(S, 'search_plugins()'), defaults },
 
-  -- browse directory
-	{ 'n', '<Leader>fn', telescope_cmd(custom_module, 'browse_dir("~/notes")'), defaults },
+  -- browse directory custom dirs
+	{ 'n', '<Leader>fn', cmd(S, 'browse_dir("~/notes")'), defaults },
 
   -- todo search
   { 'n', '<Leader>ft', '<cmd>TodoTelescope<CR>', defaults }
