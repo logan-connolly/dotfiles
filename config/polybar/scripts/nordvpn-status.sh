8e2348cbaeddb947060ec8ea4ee24a8c442254aa
Logan Connolly <logan.connolly@protonmail.com>
2021-10-06 23:43:40 +0200

    Add more whitespace for nordvpn status

--- config/polybar/scripts/nordvpn-status.sh
+++ config/polybar/scripts/nordvpn-status.sh
@@ -3,7 +3,7 @@
 STATUS=$(nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
 
 if [ "$STATUS" = "Connected" ]; then
-	echo "%{F#a9b1d6}%{A1:nordvpn d:}$(nordvpn status | grep City | cut -d ':' -f2)%{A}%{F-}"
+  echo "%{F#a9b1d6}%{A1:nordvpn d:}$(nordvpn status | grep City | cut -d ':' -f2) %{A}%{F-}"
 else
-	echo "%{F#a9b1d6}%{A1:nordvpn c:} no vpn %{A}%{F-}"
+  echo "%{F#a9b1d6}%{A1:nordvpn c:} no vpn %{A}%{F-}"
 fi
