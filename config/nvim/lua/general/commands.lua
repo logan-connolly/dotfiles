d2a57c1f11f2036b6d478140ff97af94679c7a3a
Logan Connolly <logan.connolly@protonmail.com>
2021-10-21 20:04:35 +0200

    Improve diff highlighting for tpope fugitive

--- config/nvim/lua/general/commands.lua
+++ config/nvim/lua/general/commands.lua
@@ -1,5 +1,9 @@
 -- highlight yanked text
 vim.cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
 
---- transparent background
+-- transparent background
 vim.cmd 'au ColorScheme * hi Normal ctermbg=none guibg=none'
+
+-- override tpope/fugitive colorscheme
+vim.cmd 'au VimEnter * hi diffAdded guifg=#9ece6a'
+vim.cmd 'au VimEnter * hi diffRemoved guifg=#f7768e'
