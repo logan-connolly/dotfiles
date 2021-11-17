251296963c26b689655599aa86e9fee12a4d0811
Logan Connolly <logan.connolly@protonmail.com>
2021-11-17 12:21:06 +0100

    Update general autocommands with lua helper func

--- config/nvim/lua/general/commands.lua
+++ config/nvim/lua/general/commands.lua
@@ -1,9 +1,30 @@
--- highlight yanked text
-vim.cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
+local api = vim.api
 
--- transparent background
-vim.cmd 'au ColorScheme * hi Normal ctermbg=none guibg=none'
+function nvim_create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        api.nvim_command('augroup '..group_name)
+        api.nvim_command('autocmd!')
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            api.nvim_command(command)
+        end
+        api.nvim_command('augroup END')
+    end
+end
 
--- override tpope/fugitive colorscheme
-vim.cmd 'au VimEnter * hi diffAdded guifg=#9ece6a'
-vim.cmd 'au VimEnter * hi diffRemoved guifg=#f7768e'
+local autocmds = {
+    lua_highlight = {
+      { "TextYankPost", "*", [[silent! lua vim.highlight.on_yank() {higroup="IncSearch", timeout=400}]] };
+    };
+    transparent_bg = {
+      { "ColorScheme", "*", [[hi Normal ctermbg=none guibg=none]]}
+    };
+    git_add_recolor = {
+      { "VimEnter", "*", [[hi diffAdded guifg=#9ece6a]]}
+    };
+    git_remove_recolor = {
+      { "VimEnter", "*", [[hi diffRemoved guifg=#f7768e]]}
+    }
+}
+
+nvim_create_augroups(autocmds)
