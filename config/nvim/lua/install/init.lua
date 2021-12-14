d71574a64df145552312beb5fcfb8e204a831021
Logan Connolly <logan.connolly@protonmail.com>
2021-12-14 09:56:37 +0100

    Paq api changed for 1.0 release

--- config/nvim/lua/install/init.lua
+++ config/nvim/lua/install/init.lua
@@ -1,67 +1,60 @@
 -- paq.nvim automatic install
-local install_path = vim.fn.stdpath("data") .. "/site/pack/paqs/opt/paq-nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.cmd("!git clone https://github.com/savq/paq-nvim " .. install_path)
 end
-
--- paq alias
-vim.cmd 'packadd paq-nvim'
-local paq = require('paq-nvim').paq
-paq { 'savq/paq-nvim', opt = true }
 
 -- hooks
 local submodule = 'git submodule update --init --recursive'
 local update_treesitter = function() vim.cmd(":TSUpdate") end
 
--- language support
-paq { 'nvim-treesitter/nvim-treesitter', run = update_treesitter }
-paq { 'nvim-treesitter/nvim-treesitter-refactor' }
-paq { 'neovim/nvim-lspconfig' }
-paq { 'nvim-lua/lsp-status.nvim' }
-paq { 'onsails/lspkind-nvim' }
-paq { 'dense-analysis/ale' }
+require "paq" {
+  'savq/paq-nvim';
 
--- completion
-paq { 'hrsh7th/cmp-nvim-lsp' }
-paq { 'hrsh7th/cmp-buffer' }
-paq { 'hrsh7th/nvim-cmp' }
+  -- language support
+  {'nvim-treesitter/nvim-treesitter', run = update_treesitter};
+  'nvim-treesitter/nvim-treesitter-refactor';
+  'neovim/nvim-lspconfig';
+  'nvim-lua/lsp-status.nvim';
+  'onsails/lspkind-nvim';
+  'dense-analysis/ale';
 
--- snippets
-paq { 'hrsh7th/cmp-vsnip' }
-paq { 'hrsh7th/vim-vsnip' }
+  -- completion
+  'hrsh7th/cmp-nvim-lsp';
+  'hrsh7th/cmp-buffer';
+  'hrsh7th/nvim-cmp';
 
--- general plugins
-paq { 'mattn/emmet-vim' }
-paq { 'rbgrouleff/bclose.vim' }
-paq { 'phaazon/hop.nvim' }
-paq { 'preservim/tagbar' }
-paq { 'tpope/vim-commentary' }
-paq { 'norcalli/nvim-colorizer.lua' }
-paq { 'folke/todo-comments.nvim' }
+  -- snippets
+  'hrsh7th/cmp-vsnip';
+  'hrsh7th/vim-vsnip';
 
--- git
-paq { 'tpope/vim-fugitive' }
-paq { 'lewis6991/gitsigns.nvim' }
+  -- general plugins
+  'mattn/emmet-vim';
+  'rbgrouleff/bclose.vim';
+  'phaazon/hop.nvim';
+  'preservim/tagbar';
+  'tpope/vim-commentary';
+  'norcalli/nvim-colorizer.lua';
+  'folke/todo-comments.nvim';
 
--- fuzzy finders
-paq { 'nvim-lua/popup.nvim' }
-paq { 'nvim-lua/plenary.nvim' }
-paq { 'nvim-telescope/telescope-fzy-native.nvim', run = submodule }
-paq { 'nvim-telescope/telescope.nvim' }
+  -- git
+  'tpope/vim-fugitive';
+  'lewis6991/gitsigns.nvim';
 
--- debugger plugins
-paq { 'mfussenegger/nvim-dap' }
-paq { 'mfussenegger/nvim-dap-python' }
+  -- fuzzy finders
+  'nvim-lua/popup.nvim';
+  'nvim-lua/plenary.nvim';
+  {'nvim-telescope/telescope-fzy-native.nvim', run = submodule};
+  'nvim-telescope/telescope.nvim';
 
--- theme plugins
-paq { 'kyazdani42/nvim-web-devicons' }
-paq { 'rktjmp/lush.nvim' }
-paq { 'npxbr/gruvbox.nvim' }
-paq { 'hoob3rt/lualine.nvim' }
-paq { 'tiagovla/tokyodark.nvim' }
+  -- debugger plugins
+  'mfussenegger/nvim-dap';
+  'mfussenegger/nvim-dap-python';
 
--- PaqInstall if do not exist
-local plugin_path = vim.fn.stdpath("data") .. "/site/pack/paqs/start"
-if vim.fn.empty(vim.fn.glob(plugin_path)) > 0 then
-  vim.cmd 'PaqInstall'
-end
+  -- theme plugins
+  'kyazdani42/nvim-web-devicons';
+  'rktjmp/lush.nvim';
+  'npxbr/gruvbox.nvim';
+  'hoob3rt/lualine.nvim';
+  'tiagovla/tokyodark.nvim';
+}
