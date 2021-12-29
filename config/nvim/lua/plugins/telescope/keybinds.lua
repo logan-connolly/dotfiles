f3d0bcf86903d69d7d93da556823ad5423ec8fff
Logan Connolly <logan.connolly@protonmail.com>
2021-12-29 20:39:10 +0100

    Cleanup telescope keymaps

--- config/nvim/lua/plugins/telescope/keybinds.lua
+++ config/nvim/lua/plugins/telescope/keybinds.lua
@@ -1,24 +1,15 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-local B = [['telescope.builtin'.]]
-local S = [['plugins.telescope.setup'.]]
-
-local function cmd(module, fn)
-  return '<Cmd>lua require' .. module .. fn .. ' <cr>'
-end
-
-map('n', '<leader>ff', cmd(B, "find_files({ hidden = true })"), opts)
-map('n', '<c-p>', cmd(B, 'git_files({ hidden = true })'), opts)
-map('n', '<leader>fw', cmd(B, "grep_string({ search = vim.fn.input('🔎 ') })"), opts)
-map('n', '<leader>fb', cmd(B, 'buffers()'), opts)
-map('n', '<leader>fh', cmd(B, 'help_tags()'), opts)
-map('n', '<leader>fd', cmd(S, 'search_dotfiles()'), opts)
-map('n', '<leader>fp', cmd(S, 'search_plugins()'), opts)
-map('n', '<leader>fn', cmd(S, 'browse_dir("~/notes")'), opts)
+map('n', '<leader>ff', [[<cmd>lua require'telescope.builtin'.find_files({ hidden = true })<cr>]], opts)
+map('n', '<c-p>', [[<cmd>lua require'telescope.builtin'.git_files({ hidden = true })<cr>]], opts)
+map('n', '<leader>fw', [[<cmd>lua require'telescope.builtin'.grep_string({ search = vim.fn.input('🔎 ') })<cr>]], opts)
+map('n', '<leader>fb', [[<cmd>lua require'telescope.builtin'.buffers()<cr>]], opts)
+map('n', '<leader>fh', [[<cmd>lua require'telescope.builtin'.help_tags()<cr>]], opts)
+map('n', '<leader>fp', [[<cmd>lua require'plugins.telescope.setup'.search_plugins()<cr>]], opts)
 map('n', '<leader>ft', '<cmd>TodoTelescope<cr>', opts)
-map('n', '<leader>fg', cmd(B, 'git_status()'), opts)
-map('n', '<leader>fl', cmd(B, 'git_commits()'), opts)
-map('n', '<leader>fB', cmd(B, 'git_branches()'), opts)
-map('n', '<leader>fk', cmd(B, 'keymaps()'), opts)
-map('n', '-', "<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<cr>", opts)
+map('n', '<leader>fg', [[<cmd>lua require'telescope.builtin'.git_status()<cr>]], opts)
+map('n', '<leader>fl', [[<cmd>lua require'telescope.builtin'.git_commits()<cr>]], opts)
+map('n', '<leader>fB', [[<cmd>lua require'telescope.builtin'.git_branches()<cr>]], opts)
+map('n', '<leader>fk', [[<cmd>lua require'telescope.builtin'.keymaps()<cr>]], opts)
+map('n', '-', "<cmd>lua require 'telescope'.extensions.file_browser.file_browser({cwd = require'telescope.utils'.buffer_dir()})<cr>", opts)
