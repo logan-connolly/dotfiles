4ff9fed5d7e9bc50336d8fb46ccf6da035179925
Logan Connolly <logan.connolly@protonmail.com>
2022-02-03 11:41:28 +0100

    Do not remap f in vim

--- config/nvim/lua/plugins/hop.lua
+++ config/nvim/lua/plugins/hop.lua
@@ -3,5 +3,5 @@ require 'hop'.setup{}
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map('n', 'f', [[<cmd>lua require'hop'.hint_char1()<cr>]], opts)
-map('n', 'F', [[<cmd>lua require'hop'.hint_char2()<cr>]], opts)
+map('n', 's', [[<cmd>lua require'hop'.hint_char1()<cr>]], opts)
+map('n', 'S', [[<cmd>lua require'hop'.hint_char2()<cr>]], opts)
