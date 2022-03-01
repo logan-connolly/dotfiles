b045bf89a4cbf0e5c77213342f4fa690401cff9b
Logan Connolly <logan.connolly@protonmail.com>
2022-03-02 00:17:15 +0100

    Fix rose-pine neovim theme after update

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,33 +1,35 @@
--- Set theme variant
--- Matches terminal theme if unset
--- @usage 'main' | 'moon' | 'dawn'
-vim.g.rose_pine_variant = 'moon'
+require('rose-pine').setup({
+	---@usage 'main'|'moon'
+	dark_variant = 'moon',
+	bold_vert_split = false,
+	dim_nc_background = false,
+	disable_background = false,
+	disable_float_background = false,
+	disable_italics = false,
+	---@usage string hex value or named color from rosepinetheme.com/palette
+	groups = {
+		border = 'highlight_med',
+		comment = 'muted',
+		link = 'iris',
+		punctuation = 'subtle',
 
-vim.g.rose_pine_bold_vertical_split_line = false
-vim.g.rose_pine_inactive_background = false
-vim.g.rose_pine_disable_background = false
-vim.g.rose_pine_disable_float_background = false
-vim.g.rose_pine_disable_italics = false
+		error = 'love',
+		hint = 'iris',
+		info = 'foam',
+		warn = 'gold',
 
-local p = require('rose-pine.palette')
-vim.g.rose_pine_colors = {
-	punctuation = p.subtle,
-	comment = p.subtle,
-	hint = p.iris,
-	info = p.foam,
-	warn = p.gold,
-	error = p.love,
+		headings = {
+			h1 = 'iris',
+			h2 = 'foam',
+			h3 = 'rose',
+			h4 = 'gold',
+			h5 = 'pine',
+			h6 = 'foam',
+		}
+		-- or set all headings at once
+		-- headings = 'subtle'
+	}
+})
 
-	-- Or set all headings to one colour: `headings = p.text`
-	headings = {
-		h1 = p.iris,
-		h2 = p.foam,
-		h3 = p.rose,
-		h4 = p.gold,
-		h5 = p.pine,
-		h6 = p.foam,
-	},
-}
-
--- Set colorscheme after options
-vim.cmd([[colorscheme rose-pine]])
+-- set colorscheme after options
+vim.cmd('colorscheme rose-pine')
