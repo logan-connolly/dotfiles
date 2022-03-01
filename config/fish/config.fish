68e5a73389c994d76712737cc1a1722cd2629967
Logan Connolly <logan.connolly@protonmail.com>
2022-03-01 13:15:48 +0100

    Forgot to add direnv hook to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,13 @@
 # Disable default greeting
 set fish_greeting
 
-# Setup starship shell prompt
-starship init fish | source
-
 # Source user config files
 source ~/.config/fish/user/environment.fish
 source ~/.config/fish/user/functions.fish
 source ~/.config/fish/user/aliases.fish
+
+# Setup starship shell prompt
+starship init fish | source
+
+# Setup direnv 
+direnv hook fish | source
