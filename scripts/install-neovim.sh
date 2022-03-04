15f5363173f5484d7937356ed41477872989b645
Logan Connolly <logan.connolly@protonmail.com>
2022-03-04 08:50:15 +0100

    Minor cleanup to aliases and neovim install

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -4,14 +4,14 @@ REPO_URL=https://github.com/neovim/neovim
 NEOVIM_DIR=${HOME}/.local/opt/neovim
 
 clone_repo() {
-	mkdir -p $NEOVIM_DIR
-	git clone $REPO_URL --single-branch $NEOVIM_DIR
+  mkdir -p $NEOVIM_DIR
+  git clone $REPO_URL --single-branch $NEOVIM_DIR
 }
 
 build_install() {
-	cd $NEOVIM_DIR
-	git pull
-	make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
+  cd $NEOVIM_DIR
+  git pull
+  make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
 }
 
 [ ! -d "${NEOVIM_DIR}" ] && clone_repo
