e047dc8f80385edc009afb1c984c493c4e6b92c7
Logan Connolly <logan.connolly@protonmail.com>
2022-04-13 10:38:24 +0200

    Shorten the breakpoint keymaps

--- config/nvim/lua/plugins/nvim-dap.lua
+++ config/nvim/lua/plugins/nvim-dap.lua
@@ -39,8 +39,8 @@ vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
 vim.keymap.set("n", "<F3>", ":lua require'dap'.step_over()<CR>")
 vim.keymap.set("n", "<F2>", ":lua require'dap'.step_into()<CR>")
 vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
-vim.keymap.set("n", "<leader>db", ":lua require'dap'.toggle_breakpoint()<CR>")
-vim.keymap.set("n", "<leader>dB", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 vim.keymap.set("n", "<leader>dt", ":lua require('dap-python').test_method()<CR>")
 
 -- options
