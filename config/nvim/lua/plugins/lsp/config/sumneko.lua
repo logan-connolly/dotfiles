be30acddd5ad425f9c45ad1ea9b3f3f2c250174d
Logan Connolly <logan.connolly@protonmail.com>
2022-06-23 11:45:03 +0200

    Update sumneko lsp according to documentation

--- config/nvim/lua/plugins/lsp/config/sumneko.lua
+++ config/nvim/lua/plugins/lsp/config/sumneko.lua
@@ -10,22 +10,21 @@ M.settings = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';')
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'}
+        globals = {'vim'},
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
-        }
-      }
-    }
-  }
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
 
 return M
