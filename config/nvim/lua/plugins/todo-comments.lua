fe448ecc918d7a16c4bbe1f9f55dc6b3e117d2c4
Logan Connolly <logan.connolly@protonmail.com>
2022-07-06 16:15:54 +0200

    Improve keymaps for todo comments search

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -1,16 +1,16 @@
-require 'todo-comments'.setup {}
+require('todo-comments').setup {}
 
 -- Search todos in file buffer
-vim.keymap.set('n', '<leader>tdb', function()
+vim.keymap.set('n', '<leader>ftb', function()
   vim.api.nvim_command("TodoTrouble cwd=" .. vim.fn.expand("%"))
 end)
 
 -- Search todos in the buffer's parent directory
-vim.keymap.set('n', '<leader>tdd', function()
+vim.keymap.set('n', '<leader>ftd', function()
   vim.api.nvim_command("TodoTrouble cwd=" .. vim.fn.expand "%:p:h")
 end)
 
 -- Search todos in the entire project
-vim.keymap.set('n', '<leader>tdp', function()
+vim.keymap.set('n', '<leader>ftp', function()
   vim.api.nvim_command("TodoTrouble")
 end)
