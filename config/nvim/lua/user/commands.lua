2f86c5967258ecca242eee01f0d6268ae3d11d64
Logan Connolly <logan.connolly@protonmail.com>
2022-08-29 17:13:57 +0200

    Make kitty terminal transparent by default

--- config/nvim/lua/user/commands.lua
+++ config/nvim/lua/user/commands.lua
@@ -18,6 +18,8 @@ local autocmds = {
 	},
 	transparent_bg = {
 		{ "ColorScheme", "*", [[hi Normal ctermbg=none guibg=none]] },
+		{ "ColorScheme", "*", [[hi NonText ctermbg=NONE guibg=NONE]] },
+		{ "ColorScheme", "*", [[hi EndOfBuffer ctermbg=NONE guibg=NONE]] },
 	},
 	ftplugin_settings = {
 		{ "Filetype", "dockerfile,sh,zsh", [[setlocal tabstop=2 shiftwidth=2 expandtab]] },
