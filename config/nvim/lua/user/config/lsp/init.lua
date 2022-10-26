717878172a389f546e87ba25f238016c70368376
Logan Connolly <logan.connolly@protonmail.com>
2022-10-26 17:43:32 +0200

    Add more lsp completion capabilities

--- config/nvim/lua/user/config/lsp/init.lua
+++ config/nvim/lua/user/config/lsp/init.lua
@@ -17,7 +17,35 @@ local server_configs = {
 
 -- Configure each LSP by attaching completion and capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = { "documentation", "detail", "additionalTextEdits" },
+}
+capabilities.textDocument.codeAction = {
+    dynamicRegistration = false,
+    codeActionLiteralSupport = {
+        codeActionKind = {
+            valueSet = {
+                "",
+                "quickfix",
+                "refactor",
+                "refactor.extract",
+                "refactor.inline",
+                "refactor.rewrite",
+                "source",
+                "source.organizeImports",
+            },
+        },
+    },
+}
 capabilities = cmp_lsp.default_capabilities(capabilities)
+
 for server, config in pairs(server_configs) do
 	config.capabilities = capabilities
 	lspconfig[server].setup(config)
