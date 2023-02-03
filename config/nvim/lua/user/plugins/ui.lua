0cee1fb7e144d45a2fa59e632a91dcd54e2cd372
Logan Connolly <logan.connolly@protonmail.com>
2023-02-03 23:12:03 +0100

    feat(nvim): add alpha.nvim for dashboard

--- config/nvim/lua/user/plugins/ui.lua
+++ config/nvim/lua/user/plugins/ui.lua
@@ -33,6 +33,65 @@ return {
 	-- ui components
 	{ "MunifTanjim/nui.nvim", lazy = true },
 
+	-- dashboard
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		opts = function()
+			local dashboard = require("alpha.themes.dashboard")
+			local header = {
+				[[███    ██ ███████  ██████  ██    ██ ██ ███    ███]],
+				[[████   ██ ██      ██    ██ ██    ██ ██ ████  ████]],
+				[[██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██]],
+				[[██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██]],
+				[[██   ████ ███████  ██████    ████   ██ ██      ██]],
+			}
+
+			dashboard.section.header.val = header
+			dashboard.section.buttons.val = {
+				dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
+				dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+			}
+			for _, button in ipairs(dashboard.section.buttons.val) do
+				button.opts.hl = "AlphaButtons"
+				button.opts.hl_shortcut = "AlphaShortcut"
+			end
+			dashboard.section.footer.opts.hl = "Type"
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.section.buttons.opts.hl = "AlphaButtons"
+			dashboard.opts.layout[1].val = 6
+			return dashboard
+		end,
+		config = function(_, dashboard)
+			vim.b.miniindentscope_disable = true
+
+			-- close Lazy and re-open when the dashboard is ready
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "AlphaReady",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
+
+			require("alpha").setup(dashboard.opts)
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LazyVimStarted",
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
+		end,
+	},
+
 	-- status line
 	{
 		"feline-nvim/feline.nvim",
