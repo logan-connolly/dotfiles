17702ed43831c0ac05b0be429125584c3b469ddb
Logan Connolly <logan.connolly@protonmail.com>
2023-02-09 20:39:14 +0100

    feat(nvim): run vim-test in toggleterm
    Big thanks to @will for the helper!
    
    ref: https://github.com/vim-test/vim-test/issues/619

--- config/nvim/lua/user/plugins/coding.lua
+++ config/nvim/lua/user/plugins/coding.lua
@@ -160,7 +160,22 @@ return {
 		"vim-test/vim-test",
 		event = "VeryLazy",
 		config = function()
-			vim.g["test#strategy"] = "harpoon"
+			local tt = require("toggleterm")
+			local ttt = require("toggleterm.terminal")
+
+			vim.g["test#custom_strategies"] = {
+				tterm = function(cmd)
+					tt.exec(cmd)
+				end,
+
+				tterm_close = function(cmd)
+					local term_id = 0
+					tt.exec(cmd, term_id)
+					ttt.get_or_create_term(term_id):close()
+				end,
+			}
+
+			vim.g["test#strategy"] = "tterm"
 			vim.g["test#python#pytest#options"] = "-vv"
 		end,
 		keys = {
