c98aa986e84c987a1fd6383a24dbf167f1a6a063
Logan Connolly <logan.connolly@protonmail.com>
2023-02-28 21:17:10 +0100

    fix(nvim): conflicting keybinds with bufdelete

--- config/nvim/lua/user/plugins/editor.lua
+++ config/nvim/lua/user/plugins/editor.lua
@@ -109,8 +109,8 @@ return {
 		cmd = { "TroubleToggle", "Trouble" },
 		opts = { use_diagnostic_signs = true },
 		keys = {
-			{ "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-			{ "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+			{ "<leader>tt", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+			{ "<leader>tT", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
 		},
 	},
 
