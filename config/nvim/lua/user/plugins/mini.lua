fb3fe9ecc8eeb23a516cee82b40b7e3c244f1ce8
Logan Connolly <logan.connolly@protonmail.com>
2023-02-28 21:19:19 +0100

    feat(nvim): add additional mini modules

--- config/nvim/lua/user/plugins/mini.lua
+++ config/nvim/lua/user/plugins/mini.lua
@@ -1,9 +1,16 @@
 return {
 	{
-		"echasnovski/mini.comment",
+		"echasnovski/mini.ai",
 		event = "VeryLazy",
 		config = function(_, opts)
-			require("mini.comment").setup(opts)
+			require("mini.ai").setup(opts)
+		end,
+	},
+	{
+		"echasnovski/mini.bracketed",
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("mini.bracketed").setup(opts)
 		end,
 	},
 	{
@@ -13,5 +20,26 @@ return {
 			{ "<leader>x", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
 			{ "<leader>X", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
 		},
+	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("mini.comment").setup(opts)
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("mini.pairs").setup(opts)
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("mini.surround").setup(opts)
+		end,
 	},
 }
