17f20b3b8d8ab56d82e33cf527682d49691e0faf
Logan Connolly <logan.connolly@protonmail.com>
2023-03-02 22:44:30 +0100

    feat(nvim): use noice for notifications

--- config/nvim/lua/user/plugins/ui.lua
+++ config/nvim/lua/user/plugins/ui.lua
@@ -9,11 +9,33 @@ return {
 		end,
 	},
 
-	-- simple notifier
+	-- noice notifier
 	{
-		"vigoux/notifier.nvim",
+		"folke/noice.nvim",
 		event = "VeryLazy",
-		config = true,
+		opts = {
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+		-- stylua: ignore
+		keys = {
+			{ "<leader>nl", function() require("noice").cmd("last") end, desc = "Show last notification" },
+			{ "<leader>nh", function() require("noice").cmd("history") end, desc = "Show notification history" },
+		},
 	},
 
 	-- better vim.ui
